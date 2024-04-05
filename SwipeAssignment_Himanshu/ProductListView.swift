6991e6a4329abd6ea74e91c447a36ac2ebdff029
Himanshu <h23himanshu@gmail.com>
2024-04-05 07:19:22 +0530

    Add Product List View

--- SwipeAssignment_Himanshu/ProductListView.swift
+++ SwipeAssignment_Himanshu/ProductListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ProductListView.swift
 //  SwipeAssignment_Himanshu
 //
 //  Created by Himanshu Gupta on 05/04/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProductListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ProductListView()
 }
