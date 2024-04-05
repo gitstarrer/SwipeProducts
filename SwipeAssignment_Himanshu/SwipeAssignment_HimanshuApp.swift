6c3b788f2ecbeb65731b16b0c8c9a5f9227a1a0d
Himanshu <h23himanshu@gmail.com>
2024-04-05 06:09:16 +0530

    Initial Commit

---
+++ SwipeAssignment_Himanshu/SwipeAssignment_HimanshuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAssignment_HimanshuApp.swift
+//  SwipeAssignment_Himanshu
+//
+//  Created by Himanshu Gupta on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAssignment_HimanshuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
