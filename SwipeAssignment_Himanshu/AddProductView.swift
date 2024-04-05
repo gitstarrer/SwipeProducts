1c7afcd0af56dd804c107a8b71a12724f2ed8f5c
Himanshu <h23himanshu@gmail.com>
2024-04-05 08:06:06 +0530

    add addProductView

--- SwipeAssignment_Himanshu/AddProductView.swift
+++ SwipeAssignment_Himanshu/AddProductView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct AddProductView: View {
+    @State private var name: String = ""
+    @State private var price: String = ""
+    @State private var tax: String = ""
+    @State private var selectedType = "Product"
+    let types = ["Product", "Special", "21", "sdas", "xyz"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            VStack {
+                Form {
+                    Section(header: Text("Product Name")){
+                        TextField(text: $name) {
+                            Text("Product Name")
+                                .padding()
+                        }
+                    }
+                    
+                    Section(header: Text("Product Type")){
+                        Picker("Product Type", selection: $selectedType) {
+                            ForEach(types, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Price")){
+                        TextField(text: $price) {
+                            Text("Product Selling Price")
+                                .padding()
+                        }
+                        TextField(text: $tax) {
+                            Text("Tax")
+                                .padding()
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Add Product")
+        }
     }
 }
 
