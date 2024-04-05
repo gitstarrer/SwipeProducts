26c1522e3186a1f31a11e89e55deead4db9db027
Himanshu <h23himanshu@gmail.com>
2024-04-05 08:02:19 +0530

    add AddProductView

---
+++ SwipeAssignment_Himanshu/AddProductView.swift
@@ -0,0 +1,18 @@
+//
+//  AddProductView.swift
+//  SwipeAssignment_Himanshu
+//
+//  Created by Himanshu Gupta on 05/04/24.
+//
+
+import SwiftUI
+
+struct AddProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddProductView()
+}
